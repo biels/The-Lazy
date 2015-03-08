115785c6305bbcce8fc6e20b982321a23e8131cd
biels <bielsimon2@gmail.com>
2015-03-08 13:11:30 +0100

    Actualitzacions UI

--- TheLazyClient/MainWindow.xaml.vb
+++ TheLazyClient/MainWindow.xaml.vb
@@ -1,23 +1,15 @@
 ﻿Class MainWindow
 
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        Worker.Work()
-
-    End Sub
-
-    Private Sub Image_MouseDown(sender As Object, e As MouseButtonEventArgs)
-
-    End Sub
-
     Private Sub btnClose_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles btnClose.MouseDown
         Me.Close()
     End Sub
 
-    Private Sub Image_MouseDown_1(sender As Object, e As MouseButtonEventArgs)
-
-    End Sub
-
     Private Sub btnMinimize_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles btnMinimize.MouseDown
         Me.WindowState = Windows.WindowState.Minimized
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        ' Worker.Work()
+        MsgBox("Funció no implementada")
     End Sub
 End Class
