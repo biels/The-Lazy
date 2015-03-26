af17b348ca3e03f5ee1f6a66cfffa29371676a68
biels <bielsimon2@gmail.com>
2015-03-26 21:41:50 +0100

    Modificació DB

--- TheLazyClientMVVM/Entities/UserEntity.cs
+++ TheLazyClientMVVM/Entities/UserEntity.cs
@@ -11,11 +11,15 @@ namespace TheLazyClientMVVM.Entities
         public int id { get; set; }
         public string username { get; set; }
         public string real_name { get; set; }
+        public GenderEnum gender { get; set; }
         public string email { get; set; }
         public int permission_level { get; set; }   
         public string status { get; set; }
-        public GroupEntity group { get; set; }
-        public int balance { get; set; }        
+        public string group_code { get; set; }
+        public AcademicLevelEntity academic_level { get; set; }
+        public EducationCenterEntity education_center { get; set; }
+        public int balance { get; set; }
 
+        public enum GenderEnum { M, F }
     }
 }
