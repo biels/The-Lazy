793ebf79700fed98e838fea4f7e87a4baf29edbb
biels <bielsimon2@gmail.com>
2015-03-28 00:33:05 +0100

    Actualització ElementViewer

--- TheLazyClient/Elements/ElementViewer.xaml.vb
+++ TheLazyClient/Elements/ElementViewer.xaml.vb
@@ -1,3 +1,16 @@
-﻿Public Class ElementViewer
+﻿Imports TheLazyClientMVVM
 
+Public Class ElementViewer
+    Private _Element As Entities.ElementEntity
+    Public Property Element() As Entities.ElementEntity
+        Get
+            Return _Element
+        End Get
+        Set(ByVal value As Entities.ElementEntity)
+            _Element = value
+        End Set
+    End Property
+    Public Sub LoadElement(id As Integer) 'Per editar
+        Element = DbClient.DbElementClient.getElementInfo(id)
+    End Sub
 End Class
