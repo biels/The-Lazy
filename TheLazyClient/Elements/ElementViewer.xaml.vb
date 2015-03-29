3155d7c51a709df8c4bcfc86d2f3d68bcee790b9
biels <bielsimon2@gmail.com>
2015-03-29 14:27:09 +0200

    Redisseny UI ElementViewer

--- TheLazyClient/Elements/ElementViewer.xaml.vb
+++ TheLazyClient/Elements/ElementViewer.xaml.vb
@@ -27,4 +27,19 @@ Public Class ElementViewer
 
         End If
     End Sub
+    Dim s As Boolean
+    Private Sub imgFavorites_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles imgFavorites.MouseDown
+        'Dim conv As New ImageSourceConverter()
+        'imgFavorites.Source = conv.ConvertFromString("/TheLazyClient;component/media/Icones/Star.png")
+        s = Not s
+        setStarred(s)
+    End Sub
+    Sub setStarred(starred As Boolean)
+        Dim n As String = If(starred, "Star", "Draw-Star")
+        Dim image As BitmapImage = New BitmapImage()
+        image.BeginInit()
+        image.UriSource = New Uri("pack://application:,,,/media/Icones/" + n + ".png")
+        image.EndInit()
+        imgFavorites.Source = image
+    End Sub
 End Class
--- TheLazyClient/Elements/RatingControl.xaml.vb
+++ TheLazyClient/Elements/RatingControl.xaml.vb
@@ -1,7 +1,7 @@
 ﻿Public Class RatingControl
     
     Private Sub RatingControl_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        'RenderNumbers()
+        RenderNumbers()
     End Sub
     Sub RenderNumbers()
         If cnvCanvas Is Nothing Then Exit Sub
@@ -14,25 +14,14 @@
                 .Height = 30
                 .Width = (Me.Width / 10)
                 '.Margin.Left = 
-                .Margin = New Thickness(30 * i - 38, 0, 0, 0)
+                Dim w As Double = .Width
+                If w > 0 Then
+                    .Margin = New Thickness((w) * (i - 1) - 10, 0, 0, 0)
+                End If
+
                 If sldrSlider.Value >= i Then
                     .FontSize = 18
-                    Select Case i
-                        'Case Is <= 2
-                        '    .Foreground = Brushes.Red
-                        Case Is <= 3
-                            .Foreground = Brushes.DarkRed
-                        Case Is <= 4
-                            .Foreground = Brushes.OrangeRed
-                        Case Is <= 5
-                            .Foreground = Brushes.DarkOrange
-                        Case Is <= 7
-                            .Foreground = Brushes.Orange
-                        Case Is <= 9
-                            .Foreground = Brushes.Green
-                        Case Is <= 10
-                            .Foreground = Brushes.DarkGreen
-                    End Select
+                    .Foreground = ColorForI(i)
                 End If
                 If Not (sldrSlider.Value >= i And sldrSlider.Value >= (i + 1)) Then
                     .FontWeight = FontWeights.Bold
@@ -41,8 +30,30 @@
             cnvCanvas.Children.Add(lbl)
         Next
     End Sub
-
+    Public Function ColorForI(i As Integer)
+        Select Case i
+            'Case Is <= 2
+            '    Return  Brushes.Red
+            Case Is <= 3
+                Return Brushes.DarkRed
+            Case Is <= 4
+                Return Brushes.OrangeRed
+            Case Is <= 5
+                Return Brushes.DarkOrange
+            Case Is <= 7
+                Return Brushes.Orange
+            Case Is <= 9
+                Return Brushes.Green
+            Case Is <= 10
+                Return Brushes.DarkGreen
+        End Select
+        Return Brushes.Blue
+    End Function
     Private Sub Slider_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        RenderNumbers()
+    End Sub
+
+    Private Sub RatingControl_SizeChanged(sender As Object, e As SizeChangedEventArgs) Handles Me.SizeChanged
         RenderNumbers()
     End Sub
 End Class
