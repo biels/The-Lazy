917502b3d41022fe2a8706a0992b4484d09ce257
biels <bielsimon2@gmail.com>
2015-04-06 02:16:42 +0200

    Cache bugfix

--- TheLazyClientMVVM/Cache/SubjectCache.cs
+++ TheLazyClientMVVM/Cache/SubjectCache.cs
@@ -11,16 +11,16 @@ namespace TheLazyClientMVVM.Cache
     {
         //private bool filled = false; //S'ha omplert la llista? Sinó el resultat podria ser incomplet
         private List<int> filled_academic_level_ids; //S'ha omplert la llista per aquest determinat ID? Sinó el resultat podria ser incomplet
-        private List<SubjectEntity> academic_levels { get; set; }
+        private List<SubjectEntity> subjects { get; set; }
         public SubjectCache()
         {
-            academic_levels = new List<SubjectEntity>();
+            subjects = new List<SubjectEntity>();
             filled_academic_level_ids = new List<int>();
         }
         public SubjectEntity getSubject(int id, bool direct = false)
         {
             Predicate<SubjectEntity> match = new Predicate<SubjectEntity>(e => id == e.id);
-            SubjectEntity cached = academic_levels.FirstOrDefault(e => match(e) && e.isValid());
+            SubjectEntity cached = subjects.FirstOrDefault(e => match(e) && e.isValid());
             if (cached != null && !direct) 
             {
                 Com.main.cached_query_count++;
@@ -29,16 +29,16 @@ namespace TheLazyClientMVVM.Cache
             else
             {
                 SubjectEntity tocache = DbClient.DbSubjectEditorClient.getSubjectInfo(id);
-                academic_levels.RemoveAll(match);
-                academic_levels.Add(tocache);
+                subjects.RemoveAll(match);
+                subjects.Add(tocache);
                 return tocache;
             }
         }
         public List<SubjectEntity> getSubjectFullList(int academic_level_id, bool direct = false) //Cal inicialitzar
         {
-            List<SubjectEntity> filtered = academic_levels.Where(e => e.academic_level.id == academic_level_id).ToList<SubjectEntity>();
+            List<SubjectEntity> filtered = subjects.Where(e => e.academic_level.id == academic_level_id).ToList<SubjectEntity>();
             Predicate<SubjectEntity> isvalid = new Predicate<SubjectEntity>(e => e.isValid());
-            bool allValid = filtered.TrueForAll(isvalid);
+            bool allValid = filtered.TrueForAll(isvalid) || !Com.main.config.caching_enabled;
             bool idfilled = filled_academic_level_ids.Contains(academic_level_id);
             if (allValid && !direct && idfilled)
             {
@@ -54,8 +54,8 @@ namespace TheLazyClientMVVM.Cache
         }
         public void fill(int academic_level_id)
         {
-            academic_levels.RemoveAll(e => e.academic_level.id == academic_level_id);
-            academic_levels.AddRange(DbClient.DbSubjectEditorClient.getSubjectList(academic_level_id));
+            subjects.RemoveAll(e => e.academic_level.id == academic_level_id);
+            subjects.AddRange(DbClient.DbSubjectEditorClient.getSubjectList(academic_level_id));
             filled_academic_level_ids.Add(academic_level_id);
         }
         public void fill()
