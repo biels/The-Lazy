9de3d46b8ab9c43601c9c5034aca99934f31d82a
biels <bielsimon2@gmail.com>
2015-04-09 17:43:13 +0200

    Improved stealth mode UI

--- TheLazyClient/Extra features/Stealth mode/StealthModeUI.xaml.vb
+++ TheLazyClient/Extra features/Stealth mode/StealthModeUI.xaml.vb
@@ -1,3 +1,15 @@
 ﻿Public Class StealthModeUI
 
+    Private Sub StealthModeUI_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        UpdateCheckbox()
+    End Sub
+    Private Sub chkEnabled_Checked(sender As Object, e As RoutedEventArgs) Handles chkEnabled.Click
+        UpdateCheckbox()
+    End Sub
+    Sub UpdateCheckbox()
+        Dim enb As Boolean = chkEnabled.IsChecked
+        chkEnabled.Foreground = If(enb, Brushes.Green, Brushes.IndianRed)
+        chkEnabled.Content = If(enb, "Activat", "Desactivat")
+    End Sub
+
 End Class
