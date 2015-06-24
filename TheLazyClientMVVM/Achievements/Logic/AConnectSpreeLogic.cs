a79ad2942adf61732f8c76c1657d9a495cb40951
biels <bielsimon2@gmail.com>
2015-06-24 15:59:29 +0200

    Primera classe de AcievementLogic

---
+++ TheLazyClientMVVM/Achievements/Logic/AConnectSpreeLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLazyClientMVVM.Achievements.Logic
+{
+    class AConnectSpreeLogic : AchievementLogic
+    {
+        public override string getName()
+        {
+            return "Connexió diària";
+        }
+        public override string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
